980a6d087796fa2d1911475fa4ae0b676541fd3b
NadimRai <rai_nadim@yahoo.com>
2017-05-28 18:48:42 -0700

    created association between sale and book model

--- app/models/book.rb
+++ app/models/book.rb
@@ -1,8 +1,9 @@
 class Book < ApplicationRecord
 	extend FriendlyId
 	friendly_id :name, use: :slugged
-	
+
 	belongs_to :user
+	has_many :sales
 
 	has_attached_file :image, styles: { medium: "300x300>", thumb: "100x100>" }, default_url: "/images/:style/missing.png"
   	validates_attachment_content_type :image, content_type: /\Aimage\/.*\z/
