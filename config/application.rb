57d1e633e14d86775bdfda0857283405af140b75
NadimRai <rai_nadim@yahoo.com>
2017-05-28 17:47:18 -0700

    ability to display image in book index and show page

--- config/application.rb
+++ config/application.rb
@@ -21,7 +21,8 @@ module TextBook2
     # 		bucket: ENV['AWS_BUCKET'],
     # 		access_key_id: ENV['AWS_ACCESS_KEY_ID'],
     # 		secret_access_key: ENV['AWS_SECRET_ACCESS_KEY'],
-    # 		s3_region: ENV['AWS_REGION']
+    # 		s3_region: ENV['AWS_REGION'],
+    # 		s3_host_name: 's3-us-west-2.amazonaws.com'
     # 	}
     # }
   end
